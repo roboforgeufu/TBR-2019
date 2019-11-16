ff293931a24b8c50de0c8b4154fbab697e945245
Arthur Borges <abrothguer@gmail.com>
2019-11-16 16:32:57 -0200

    Testes de funções do Otávio, DriveBase, proc killer e shouting thread

--- main.py
+++ main.py
@@ -1,15 +1,57 @@
 #!/usr/bin/env pybricks-micropython
 
+import os
+import signal
+import threading
+
 from pybricks import ev3brick as brick
-from pybricks.parameters import Button, Port
+from pybricks.parameters import Button, Port, SoundFile
 from pybricks.tools import print, wait
 
 from robot import Robot
 
+
+# TODO: ARTHUR FAZER CONVERSÃO GRAUS -> CM
+
+
+
 def test_catch(robot):
-    triton.catch()
+    robot.catch()
     wait(1000)
-    triton.catch(release = True)
+    robot.catch(release = True)
+
+def test_walk_base(robot):
+    print('Walking ahead')
+    robot.walk2(200, 0)
+    wait(1000)
+    robot.stop()
+
+    print('Going back...')
+    robot.walk2(-200, 0)
+    wait(1000)
+    robot.stop()
+
+def test_walk(robot):
+    print('Walking ahead')
+    robot.walk(aFuncao = -0.04, bFuncao = 4, cFuncao = 5, graus = 1000, intervOscilacao=5)
+    robot.stop()
+    wait(1000)
+
+    print('Going back...')
+    robot.walk(aFuncao = 0.04, bFuncao = -4, cFuncao = -5, graus = -1000, intervOscilacao=5)
+    robot.stop()
+    wait(1000)
+
+def test_turn(robot):
+    print('Turning 90 deg')
+    robot.turn(aFuncao = -0.04, bFuncao = 4, cFuncao = 5, grausCurva = 90)
+    robot.stop()
+    wait(1000)
+
+    print('Turning 180 deg')
+    robot.turn(aFuncao = -0.04, bFuncao = 4, cFuncao = 5, grausCurva = 180)
+    robot.stop()
+    wait(1000)
 
 def start_robot():
     print('Starting...')
@@ -17,15 +59,31 @@ def start_robot():
     
     # Code goes here
     test_catch(triton)
+    test_walk(triton)
+    test_turn(triton)
 
     print('Goodbye...')
-    wait(10000)
+    wait(1000)
 
 def sing():
     print('Singing in the rain...')
     wait(1000)
 
+def shouting():
+    while True:
+        brick.sound.file(SoundFile.SHOUTING)
+        wait(500)
+
 def main ():
+
+    procs = [item.split()[0] for item in os.popen('ps -e').read().splitlines()[4:] if 'pybricks-micropython' in item.split()]
+    for proc in procs:
+        if proc != os.getpid():
+            os.kill(proc, signal.SIGTERM)
+
+    brick.sound.beep()
+    shout = threading.Thread(target = shouting)
+    shout.start()
 
     while True:
         while not any(brick.buttons()):
--- robot.py
+++ robot.py
@@ -1,5 +1,7 @@
 from pybricks.ev3devices import Motor, ColorSensor
-from pybricks.tools import wait, print
+from pybricks.tools import wait, print, StopWatch
+from pybricks.parameters import Stop
+from pybricks.robotics import DriveBase
 
 import constants as const
 
@@ -11,13 +13,129 @@ class Robot():
         self.rmotor = Motor(rmport)
         self.claw = Motor(clport)
         self.recog = ColorSensor(csport)
+        self.stopwatch = StopWatch()
+        self.base = DriveBase(self.lmotor, self.rmotor, 56, 190)
 
-    def foward(self):
-        pass
+    def walk2(self, speed, steering):
+        
+        self.base.drive(speed, steering)
+
+    def walk(self, aFuncao = 0, bFuncao = 0, cFuncao = 0, graus = 0, intervOscilacao = 15, insideReset = True):
+        print('Walking...')
+        if insideReset:
+            self.lmotor.reset_angle(0)
+            self.rmotor.reset_angle(0)
+        velocDir = 0
+        velocEsq = 0
+        while True:
+            #print(self.lmotor.angle(), self.rmotor.angle())
+            #Funcao que descreve a velocidade em funcao da porcentagem do deslocamento ja realizado
+            percDeslocado = (self.lmotor.angle()/graus)*100
+            velocEsq = (aFuncao * (percDeslocado ** 2) + bFuncao * percDeslocado + cFuncao)*10
+            velocDir = velocEsq
+            
+            #Teto
+            if velocEsq > 900:
+                velocEsq = 900
+            if velocDir > 900:
+                velocDir = 900
+
+            diferenca_EsqDir = abs(self.lmotor.angle()) - abs(self.rmotor.angle())
+            if abs(diferenca_EsqDir) > 3:
+                if diferenca_EsqDir > 0:
+                    #self.lmotor andou mais, joga mais velocidade no self.rmotor
+                    velocEsq = velocEsq * (1 - (intervOscilacao/100))
+                    velocDir = velocDir * (1 + (intervOscilacao/100))
+                else:
+                    #self.rmotor andou mais, joga mais velocidade no self.lmotor
+                    velocEsq = velocEsq * (1 + (intervOscilacao/100))
+                    velocDir = velocDir * (1 - (intervOscilacao/100))
+            self.lmotor.run(velocEsq)
+            self.rmotor.run(velocDir)
+            if (abs(velocEsq) < 50 and abs(velocDir) < 50) or percDeslocado >= 99:
+                break
+
+    def turn(self, aFuncao, bFuncao, cFuncao, grausCurva, fix = True):
+        print('Turning...')
+        self.lmotor.reset_angle(0)
+        self.rmotor.reset_angle(0)
+
+        #P/ calibrar
+        K = 180
+        grausMotor = K * grausCurva / 90
+        
+        mediaPercorrida = 0
+        while mediaPercorrida < 99:  
+            #Calculo da velocidade de ambos os motores em funcao da media do deslocamento percorrido por cada um
+            percPercorridoEsq = (abs(self.lmotor.angle())/grausMotor)*100
+            percPercorridoDir = (abs(self.rmotor.angle())/grausMotor)*100
+            mediaPercorrida = (percPercorridoDir + percPercorridoEsq)/2.0
+            
+            velocidade = (aFuncao * (mediaPercorrida ** 2) + bFuncao * mediaPercorrida + cFuncao)*10
+            
+            self.lmotor.run(velocidade)
+            self.rmotor.run(-velocidade)
+        self.lmotor.stop()
+        self.rmotor.stop()
+
+        if fix:
+            while True:
+                #Para o motor Esquerdo:
+                difEsq = abs(self.lmotor.angle()) - grausMotor
+                if abs(difEsq) > 3:
+                    #A diferenca eh consideravel
+                    if difEsq > 0:
+                        #Andou mais do que devia
+                        sinalEsq = -1 * (self.lmotor.angle()/abs(self.lmotor.angle())) #Deve se movimentar no sentido contrario ao mov anterior
+                    else:
+                        #Andou menos do que devia
+                        sinalEsq = (self.lmotor.angle()/abs(self.lmotor.angle())) #Deve se movimentar no mesmo sentido do movimento anterior
+                    velocEsq = sinalEsq * (2*abs(difEsq)+50)
+                else:
+                    #A diferenca eh irrelevante
+                    velocEsq = 0
+
+                #Para o motor Direito:
+                difDir = abs(self.rmotor.angle()) - grausMotor
+                if abs(difDir) > 3:
+                    #A diferenca eh consideravel
+                    if difDir > 0:
+                        #Andou mais do que devia
+                        sinalDir = -1 * (self.rmotor.angle()/abs(self.rmotor.angle())) #Deve se movimentar no sentido contrario ao mov anterior
+                    else:
+                        #Andou menos do que devia
+                        sinalDir = (self.rmotor.angle()/abs(self.rmotor.angle())) #Deve se movimentar no mesmo sentido do movimento anterior
+                    velocDir = sinalDir * (2*abs(difDir)+50)
+                else:
+                    #A diferenca eh irrelevante
+                    velocDir = 0
+                
+                #Por fim, manda a velocidade calculada para os motores
+                self.lmotor.run(velocEsq)
+                self.rmotor.run(velocDir)
+
+                if (difDir not in range(-3, 4)) or (difEsq not in range(-3, 4)): #Se qualquer dos motores estiver fora do intervalo seguro, entao ele ainda nao comeca a contar
+                    self.stopwatch.reset() # Nao comecar a contar <=> resetar constantemente o self.stopwatch
+                
+                #print("Tempo:", self.stopwatch.time(), "\ Vel:", velocDir, velocEsq)
+                #print(difDir, difEsq)
+                if self.stopwatch.time() > 300: #Caso passem 400 ms sem resetar o self.stopwatch, ou seja, 300 ms com ambos os motores na zona segura
+                    print("E:", self.lmotor.angle(), "D:", self.rmotor.angle())
+                    print(grausMotor)
+                    print()
+                    break
+
 
     def catch(self, release = False):
         print('Catching...')
         self.claw.run((-1 if release else 1) * const.CLAW_SP)
         wait(const.CLAW_WT)
+        self.claw.stop()
 
 
+    def stop(self):
+
+        print('Stoping...')
+        self.base.stop(stop_type=Stop.BRAKE)
+        self.lmotor.stop(Stop.BRAKE)
+        self.rmotor.stop(Stop.BRAKE)
